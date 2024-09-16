97c3d44fbb5ce14be133304d9ad745e92e1422d7
NoStCh <jschulfer1498@student.pps.net>
2024-09-16 15:08:40 -0700

    Coins, GameManager, and Bullshit

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Coins", menuName = "Scriptable Objects/Coins")]
+public class Coins : MonoBehaviour
+{
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+
+    }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            gm.coins += 1;
+            print(gm.coins);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "Scriptable Objects/GameManager")]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    public int coins;
+
+    private void Awake()
+    {
+        if (gm != null && gm != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gm = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
+
