eaa724f7ad116fd4b860b2949733ec0cef22973b
NoStCh <jschulfer1498@student.pps.net>
2024-10-10 15:31:18 -0700

    æẽĩœũ

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,28 +1,44 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 public class Turret : MonoBehaviour
 {
+    private float firerate;
+    private double cooldown;
+    private GameObject player = null;
     public GameObject projectileClone;
     public Vector3 spawnPos;
-    private int maxProjectectiles;
     void Start()
     {
+        firerate = 1;
+        cooldown = 0.5;
         spawnPos = new Vector3(transform.position.x, transform.position.y + 0.5f, 0);
+    }
+
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (cooldown <= 0 && player != null)
+        {
+            PlayerController pc = player.GetComponent<PlayerController>();
+            Instantiate(projectileClone, spawnPos, Quaternion.identity);
+            Projectile clonescript = projectileClone.GetComponent<Projectile>();
+            clonescript.playerController = pc;
+            cooldown = firerate;
+        }
     }
     private void OnTriggerStay2D(Collider2D other)
     {
+        player = other.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (maxProjectectiles < 5)
-            {
-                Instantiate(projectileClone, spawnPos, Quaternion.identity);
-                Projectile cloneScript = projectileClone.GetComponent<Projectile>();
-                maxProjectectiles += 1;
-            }
-            else
-            {
-                maxProjectectiles = 0;
-            }
+            player = null;
         }
     }
 }
