e1745ee131c46a9ca10f8aff2feefd2a3e7eeb48
NoStCh <jschulfer1498@student.pps.net>
2024-10-18 15:09:18 -0700

    awawwawawaawawwaawwawawawawawawawawwawawawwawawawawawwawwaawwawawwwawwawaawawawwawawwawawwawawawwwawaawwawwawawawawawawwawawawawawwaawwwawawwaawaaawawawaawawwaawawawawwaawaawawaawwawaaawawawawaawawaw

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Vector3 target;
+    private Vector3 curent;
+    private Vector3 direction;
+    
+    private int moveTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
