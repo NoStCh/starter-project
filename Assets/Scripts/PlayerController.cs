8df6f01b6adfe805996f89698ff42212f1506378
NoStCh <jschulfer1498@student.pps.net>
2024-11-20 15:07:25 -0800

    ghftfytfytf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,17 @@ public class PlayerController : MonoBehaviour
     public float playerX;
     public float playerY;
     public bool overworld; 
+    private Collider2D PlayerCollider;
+    public float length;
+    private Rigidbody2D rb;
+    private float JumpForce;
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        PlayerCollider = GetComponent<Collider2D>();
+        length = 0.9f;
+        JumpForce = 350f;
+        
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
 
@@ -34,6 +43,18 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        RaycastHit2D leftRay = Physics2D.Raycast(new Vector2(transform.position.x - PlayerCollider.bounds.center.x, transform.position.y), Vector2.down, length);
+        Debug.DrawRay(new Vector2(transform.position.x - PlayerCollider.bounds.center.x, transform.position.y), Vector2.down * length, Color.red);
+        RaycastHit2D rightRay = Physics2D.Raycast(new Vector2(transform.position.x + PlayerCollider.bounds.center.x, transform.position.y), Vector2.down, length);
+        Debug.DrawRay(new Vector2(transform.position.x + PlayerCollider.bounds.center.x, transform.position.y), Vector2.down * length, Color.red);
+        
+        print(leftRay);
+        
+        if ((leftRay.collider != null || rightRay.collider != null) && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * JumpForce);
+        }
+        
         playerX = transform.position.x;
         playerY = transform.position.y;
         
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -2,28 +2,47 @@ using UnityEngine;
 
 public class PlayerController2 : MonoBehaviour
 {
-    private Collider2D PlayerCollider;
+    Collider2D collider2d;
+    Rigidbody2D rb;
     public float length;
-    private Rigidbody2D rb;
-    private float JumpForce;
+    public int jump;
+
     void Start()
     {
+        jump = 0;
         rb = GetComponent<Rigidbody2D>();
-        PlayerCollider = GetComponent<Collider2D>();
-        length = 0.9f;
-        JumpForce = 350f;
+        collider2d = GetComponent<Collider2D>();
+        length = 1f; // Length of the ray to check for ground
     }
 
     void Update()
     {
-        RaycastHit2D leftRay = Physics2D.Raycast(new Vector2(transform.position.x - PlayerCollider.bounds.center.x, transform.position.y), Vector2.down, length);
-        Debug.DrawRay(new Vector2(transform.position.x - PlayerCollider.bounds.center.x, transform.position.y), Vector2.down * length, Color.red);
-        RaycastHit2D rightRay = Physics2D.Raycast(new Vector2(transform.position.x + PlayerCollider.bounds.center.x, transform.position.y), Vector2.down, length);
-        Debug.DrawRay(new Vector2(transform.position.x + PlayerCollider.bounds.center.x, transform.position.y), Vector2.down * length, Color.red);
-
-        if ((leftRay.collider != null || rightRay.collider != null) && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(transform.up * JumpForce);
+            jump = 1;  // Set jump flag when Space is pressed
+        }
+        else
+        {
+            jump = 0;  // Reset jump flag when Space is released
+        }
+
+        // Raycasts to check if the player is grounded
+        Vector2 leftRayOrigin = new Vector2(transform.position.x - collider2d.bounds.extents.x, transform.position.y);
+        Vector2 rightRayOrigin = new Vector2(transform.position.x + collider2d.bounds.extents.x, transform.position.y);
+
+        // Perform the raycasts downward
+        RaycastHit2D leftRay = Physics2D.Raycast(leftRayOrigin, Vector2.down, length);
+        RaycastHit2D rightRay = Physics2D.Raycast(rightRayOrigin, Vector2.down, length);
+
+        // Draw the rays for debugging (only draw in Scene view, not in game)
+        Debug.DrawRay(leftRayOrigin, Vector2.down * length, Color.red);
+        Debug.DrawRay(rightRayOrigin, Vector2.down * length, Color.red);
+
+        // Check if either ray hits a collider (indicating ground)
+        if ((leftRay.collider != null || rightRay.collider != null) && jump == 1)
+        {
+            // If the ray hits something and the player presses jump, apply the jump force
+            rb.AddForce(Vector2.up * 350); // Jump force
         }
     }
 }
