891ac5746e46a41c65a45a888a7f15945660d038
NoStCh <jschulfer1498@student.pps.net>
2024-12-11 15:19:45 -0800

    Update PlayerController2.cs
    fytfjdxhj

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -6,9 +6,10 @@ public class PlayerController2 : MonoBehaviour
     Rigidbody2D rb;
     public float length;
     public int jump;
-
+    public bool slam;
     void Start()
     {
+        slam = false;
         jump = 0;
         rb = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<Collider2D>();
@@ -29,15 +30,32 @@ public class PlayerController2 : MonoBehaviour
         // Raycasts to check if the player is grounded
         Vector2 leftRayOrigin = new Vector2(transform.position.x - collider2d.bounds.extents.x, transform.position.y);
         Vector2 rightRayOrigin = new Vector2(transform.position.x + collider2d.bounds.extents.x, transform.position.y);
+        Vector2 lRayOrigin = new Vector2(transform.position.x + collider2d.bounds.extents.x + 0.1f, transform.position.y - 0.8f);
+        Vector2 rRayOrigin = new Vector2(transform.position.x + collider2d.bounds.extents.x + 0.1f, transform.position.y - 0.8f);
 
         // Perform the raycasts downward
         RaycastHit2D leftRay = Physics2D.Raycast(leftRayOrigin, Vector2.down, length);
         RaycastHit2D rightRay = Physics2D.Raycast(rightRayOrigin, Vector2.down, length);
-
+        RaycastHit2D lhorzRay = Physics2D.Raycast(lRayOrigin, Vector2.left, length);
+        RaycastHit2D rhorzRay = Physics2D.Raycast(rRayOrigin, Vector2.left, length);
         // Draw the rays for debugging (only draw in Scene view, not in game)
         Debug.DrawRay(leftRayOrigin, Vector2.down * length, Color.red);
         Debug.DrawRay(rightRayOrigin, Vector2.down * length, Color.red);
+        Debug.DrawRay(lRayOrigin, Vector2.left * length, Color.red);
+        Debug.DrawRay(lRayOrigin, Vector2.left * length, Color.red);
 
+        if ((leftRay.collider != null || rightRay.collider != null) && (lhorzRay.collider != null || rhorzRay.collider != null) && slam)
+        {
+            if (lhorzRay.collider != null)
+            {
+                
+            }
+            else
+            {
+                a
+            }
+        }
+        
         // Check if either ray hits a collider (indicating ground)
         if ((leftRay.collider != null || rightRay.collider != null) && jump == 1)
         {
